61fb1f8d3f8328da2c71d8b3ca6514ebdfce1f64
Jacob Hegna <jacobhegna@gmail.com>
2017-10-02 23:59:58 -0500

    Remove testing code. Make answer feedback show the full question.

--- quiz.php
+++ quiz.php
@@ -1,35 +1,35 @@
 <?php
 
-$_POST["q1"] = "a";
-$_POST["q2"] = "c";
-$_POST["q3"] = "c";
-$_POST["q4"] = "r";
-
 $questions[1] = "Q1";
 $questions[2] = "Q2";
 $questions[3] = "Q3";
 $questions[4] = "Q4";
+
+$answers['a'] = "A";
+$answers['b'] = "B";
+$answers['c'] = "C";
+$answers['d'] = "D";
 
 $answer_key[1] = 'a';
 $answer_key[2] = 'b';
 $answer_key[3] = 'c';
 $answer_key[4] = 'd';
 
-$answers[1] = $_POST["q1"];
-$answers[2] = $_POST["q2"];
-$answers[3] = $_POST["q3"];
-$answers[4] = $_POST["q4"];
+$answer_user[1] = $_POST["q1"];
+$answer_user[2] = $_POST["q2"];
+$answer_user[3] = $_POST["q3"];
+$answer_user[4] = $_POST["q4"];
 
 $score = 0;
 $document = "";
 
 for($i = 1; $i <= 4; $i++) {
-    $document .= "Question " . $i . ": " . $questions[$i] . "<br>";
-    $document .= "You answered: " . $answers[$i] . "<br>";
-    $document .= "Correct answer: " . $answer_key[$i] . "<br>";
+    $document .= "Question " . $i . ": " . $question_user[$i]         . "<br>";
+    $document .= "You answered: "        . $answers[$answer_user[$i]] . "<br>";
+    $document .= "Correct answer: "      . $answers[$answer_key[$i]]  . "<br>";
 
     // If they got this question correct
-    if($answers[$i] == $answer_key[$i]) {
+    if($answer_user[$i] == $answer_key[$i]) {
         $score++;
     }
 
